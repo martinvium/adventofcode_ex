de06801db4b20d3de3528c90b31e8d9d4ff6a22a
Martin Vium <martin@codefuss.com>
2021-11-14 10:33:06 +0100

    Day 1 part 1

---
+++ test/aoc/year_2020/day_01/report_repair_test.exs
@@ -0,0 +1,27 @@
+defmodule Aoc.Year2020.Day01.ReportRepairTest do
+  use Aoc.DayCase
+  doctest Aoc.Year2020.Day01.ReportRepair, import: true
+
+  alias Aoc.Year2020.Day01.ReportRepair
+
+  describe "part_1/1" do
+    @tag day: 01, year: 2020
+    test "input", %{input: input} do
+      result = input |> ReportRepair.part_1()
+      assert result |> Enum.sum == 2020
+      IO.inspect result
+      IO.puts "=> #{Enum.reduce(result, fn val, sum -> sum * val end)}"
+    end
+  end
+
+  describe "part_2/1" do
+    test "examples" do
+
+    end
+
+    @tag day: 01, year: 2020
+    test "input", %{input: input} do
+      assert input |> ReportRepair.part_2() == input
+    end
+  end
+end
