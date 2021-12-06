eae8781e446293b01d73de5bc52bbe8e71f5ddf8
Martin Vium <martin@codefuss.com>
2021-12-06 08:35:04 +0100

    tmp day 4

---
+++ test/aoc/year_2021/day_04/giant_squid_test.exs
@@ -0,0 +1,24 @@
+defmodule Aoc.Year2021.Day04.GiantSquidTest do
+  use Aoc.DayCase
+  doctest Aoc.Year2021.Day04.GiantSquid, import: true
+
+  alias Aoc.Year2021.Day04.GiantSquid
+
+  describe "part_1/1" do
+    @tag day: 04, year: 2021
+    test "input", %{input: input} do
+      assert input |> GiantSquid.part_1() == []
+    end
+  end
+
+  describe "part_2/1" do
+    test "examples" do
+
+    end
+
+    @tag day: 04, year: 2021
+    test "input", %{input: input} do
+      assert input |> GiantSquid.part_2() == input
+    end
+  end
+end
