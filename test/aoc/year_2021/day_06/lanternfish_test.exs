3f33a6ea6d6ca8b2273b794d749e30d4a4b330dc
Martin Vium <martin@codefuss.com>
2021-12-06 09:18:29 +0100

    Day 6 part 1

---
+++ test/aoc/year_2021/day_06/lanternfish_test.exs
@@ -0,0 +1,20 @@
+defmodule Aoc.Year2021.Day06.LanternfishTest do
+  use Aoc.DayCase
+  doctest Aoc.Year2021.Day06.Lanternfish, import: true
+
+  alias Aoc.Year2021.Day06.Lanternfish
+
+  describe "part_1/1" do
+    @tag day: 06, year: 2021
+    test "input", %{input: input} do
+      assert input |> Lanternfish.part_1() |> length() == 389_726
+    end
+  end
+
+  describe "part_2/1" do
+    @tag day: 06, year: 2021, timeout: :infinity
+    test "input", %{input: input} do
+      assert input |> Lanternfish.part_2() == 1
+    end
+  end
+end
