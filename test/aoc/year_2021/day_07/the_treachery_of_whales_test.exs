edd81776dc5868322cf5c6b9c6568bfa6fe63700
Martin Vium <martin@codefuss.com>
2021-12-07 21:04:12 +0100

    Day 7 part 1

---
+++ test/aoc/year_2021/day_07/the_treachery_of_whales_test.exs
@@ -0,0 +1,20 @@
+defmodule Aoc.Year2021.Day07.TheTreacheryofWhalesTest do
+  use Aoc.DayCase
+  doctest Aoc.Year2021.Day07.TheTreacheryofWhales, import: true
+
+  alias Aoc.Year2021.Day07.TheTreacheryofWhales
+
+  describe "part_1/1" do
+    @tag day: 07, year: 2021
+    test "input", %{input: input} do
+      assert input |> TheTreacheryofWhales.part_1() == 331_067
+    end
+  end
+
+  describe "part_2/1" do
+    @tag day: 07, year: 2021
+    test "input", %{input: input} do
+      assert input |> TheTreacheryofWhales.part_2() == input
+    end
+  end
+end
