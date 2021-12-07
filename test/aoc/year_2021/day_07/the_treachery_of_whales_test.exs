3f0c7613e64ec99e6518b69b4cb9d3416b1f9db5
Martin Vium <martin@codefuss.com>
2021-12-07 22:35:55 +0100

    Part 2 is not right :(

--- test/aoc/year_2021/day_07/the_treachery_of_whales_test.exs
+++ test/aoc/year_2021/day_07/the_treachery_of_whales_test.exs
@@ -14,7 +14,7 @@ defmodule Aoc.Year2021.Day07.TheTreacheryofWhalesTest do
   describe "part_2/1" do
     @tag day: 07, year: 2021
     test "input", %{input: input} do
-      assert input |> TheTreacheryofWhales.part_2() == input
+      assert input |> TheTreacheryofWhales.part_2() == 1
     end
   end
 end
